d9c9fea8aba91c24d5267860634389cc0e974bfc
LatiDor <latiefad@icloud.com>
2023-09-26 17:10:38 +0100

    first commit

---
+++ diamond/src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+
+function App() {
+   const shot = () =>{
+    alert("GreatShot!")
+  }
+
+  function Yay(){
+
+  }
+  
+  return (
+    <div className="App">
+      <h1>Diamond
+      </h1>
+      <p>This is an exmaple of an event binding</p>
+      <button onClick={shot}>take the shot</button>
+
+      <div className="Yay">
+      <h1>Diamond
+      </h1>
+      <p>This is an exmaple of an event binding</p>
+      <button onClick={Apple}>take the shot</button>
+
+      <button onClick={Joint}>take the shot</button>
+
+      <button onClick={shot}>take the shot</button>
+     
+<
+
+</div>
+
+  );
+}
+
+export default App;
